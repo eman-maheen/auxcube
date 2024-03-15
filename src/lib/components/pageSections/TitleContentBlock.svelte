8d7d145f7ae5c4e61a0c12f85cb49405b7de1180
Eman Maheen <eman.maheen@auxcube.com>
2024-03-15 18:09:31 +0500

    feat: layout/wrapper components

---
+++ src/lib/components/pageSections/TitleContentBlock.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  export let heading: string;
+  export let emphasis: string;
+  export let content: string;
+
+  function decorateHeading(
+    heading: string,
+    emphasis: string
+  ): { reducedHeading: string; highlightedPart: string } {
+    const reducedHeading = heading.replace(emphasis, "");
+    return {
+      reducedHeading: reducedHeading.trim(),
+      highlightedPart: emphasis.trim(),
+    };
+  }
+
+  const { reducedHeading, highlightedPart } = decorateHeading(
+    heading,
+    emphasis
+  );
+</script>
+
+<div class="text-start mb-10 mx-5">
+  <h3 class="bannerHeading">
+    {reducedHeading}<span class="text-primary">{` ${highlightedPart}`}</span>
+  </h3>
+  {content}
+</div>
---
+++ src/lib/components/pageSections/TwoColsWrapper.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  let showMobileMenu: boolean;
+  let screenWidth = 0;
+  $: {
+   if (screenWidth > 768) showMobileMenu = false 
+   else showMobileMenu = true;
+ }
+</script>
+<svelte:window bind:innerWidth={screenWidth} />
+{#if showMobileMenu}
+ <div class="heroContent" >
+   <slot />
+   <slot name="rightColumn" />
+   <slot name="leftColumn" />
+ </div>
+{:else}
+ <div class="heroContent">
+   <div class="flex flex-col flex-1 bannerText border-r-2 border-zinc-700 md:px-5">
+     <slot name="leftColumn" />
+   </div>
+   <div class="bannerText flex-2 md:w-3/5 items-center self-start">
+     <div class="flex flex-col md:px-5">
+       <slot name="rightColumn" />
+     </div>
+     <slot />
+   </div>
+ </div>
+{/if}
+
