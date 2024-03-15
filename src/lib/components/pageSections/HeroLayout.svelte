d9628e92bb97f357cc9c23141c6802ec8d96a203
Eman Maheen <eman.maheen@auxcube.com>
2024-03-15 18:09:31 +0500

    feat: Content and Layout Components for Consultaion and Engagement Models

---
+++ src/lib/components/pageSections/HeroLayout.svelte
@@ -0,0 +1,11 @@
+<div class="heroContent">
+  <div class="flex flex-col flex-1 bannerText border-r-2 border-zinc-700 px-5">
+    <slot name="leftColumn" />
+  </div>
+  <div class="bannerText flex flex-row flex-2 md:w-3/5 items-center self-start">
+    <div class="flex flex-col px-10">
+      <slot name="rightColumn" />
+    </div>
+    <slot />
+  </div>
+</div>
