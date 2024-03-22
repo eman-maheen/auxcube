deb3cd9467fc306cb0244ac13db0f50d78e21520
Eman Maheen <eman.maheen@auxcube.com>
2024-03-22 16:45:09 +0500

    feat: main page cleanup

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-    import { page } from "$app/stores";
   import Footer from "$lib/components/Footer.svelte";
   import Navbar from "$lib/components/Navbar.svelte";
   import type { NavLink } from "$lib/content/site";
@@ -23,28 +22,34 @@
   <script>
     (function (w, d, s, l, i) {
       w[l] = w[l] || [];
-      w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+      w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
       var f = d.getElementsByTagName(s)[0],
         j = d.createElement(s),
-        dl = l != 'dataLayer' ? '&l=' + l : '';
+        dl = l != "dataLayer" ? "&l=" + l : "";
       j.async = true;
-      j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+      j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
       f.parentNode.insertBefore(j, f);
-    })(window, document, 'script', 'dataLayer', 'GTM-MZMDGRKM');
+    })(window, document, "script", "dataLayer", "GTM-MZMDGRKM");
   </script>
 </svelte:head>
 <!-- Google Tag Manager (noscript) -->
-<noscript><iframe title="gtmframe" src="https://www.googletagmanager.com/ns.html?id=GTM-MZMDGRKM"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<noscript
+  ><iframe
+    title="gtmframe"
+    src="https://www.googletagmanager.com/ns.html?id=GTM-MZMDGRKM"
+    height="0"
+    width="0"
+    style="display:none;visibility:hidden"
+  ></iframe></noscript
+>
 <!-- End Google Tag Manager (noscript) -->
 
 <!-- NOTE: Why >> <div style="display: contents" class="h-full overflow-hidden"> -->
 <svelte:window bind:innerWidth={screenWidth} />
-
 <Navbar bind:currentMenu bind:showMobileMenu />
 <div id="appShell" class:brightness-25={blurDesktop || blurMobile}>
-  <main id="pageSlot">
+  <main id="pageSlot" class="content">
     <slot />
   </main>
+  <Footer />
 </div>
-<Footer />
